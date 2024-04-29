04767380e5ef5240f8e9d87f0aace1503e4aed1c
JustinvdBerg04 <justinvandenberg04@gmail.com>
2024-04-29 11:27:40 +0200

    Navigation

---
+++ src/main/java/m2h2/Navigation/Pages/OrderPicker.java
@@ -0,0 +1,11 @@
+package m2h2.Navigation.Pages;
+
+import javax.swing.*;
+
+public class OrderPicker extends JPanel {
+
+    public OrderPicker(){
+        JLabel label = new JLabel("Je bent op de order picker page");
+        add(label);
+    }
+}
