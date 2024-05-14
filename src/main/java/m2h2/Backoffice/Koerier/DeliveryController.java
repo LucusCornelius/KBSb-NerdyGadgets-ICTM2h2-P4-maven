e6f8b34181c692d2206f3a8f969acce65bceca57
JustinvdBerg04 <justinvandenberg04@gmail.com>
2024-05-14 17:36:58 +0200

    Dialog Koerier

--- src/main/java/m2h2/Backoffice/Koerier/DeliveryController.java
+++ src/main/java/m2h2/Backoffice/Koerier/DeliveryController.java
@@ -31,13 +31,14 @@ public class DeliveryController implements ActionListener {
         initComponents();
         this.mainPanel = mainPanel;
         this.route = Route.getRoute(routeID);
-        orders =route.getOrders();
+        orders = route.getOrders();
 
         setDeliveryPanel();
     }
 
     public void setDeliveryPanel() {
         mainPanel.removeAll();
+//        mainPanel.setBackground(new Color(0,0,0, 100));
         mainPanel.setLayout(new GridLayout(5, 1));
 
         jLabel1.setFont(new Font("Segoe UI Semibold", 1, 24));
@@ -108,11 +109,11 @@ public class DeliveryController implements ActionListener {
         return sp;
     }
 
-    public Object[][] getTableData(){
+    public Object[][] getTableData() {
         Object[][] data = new Object[getOrdersTableSize()][5];
         int rowIndex = 0;
-        for (int i = 0; i < orders.size(); i++){
-            if (orders.get(i).getOrderLines().size() == 1){
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.get(i).getOrderLines().size() == 1) {
                 data[rowIndex] = orders.get(i).getDatalineRoute();
                 System.out.println(data);
                 rowIndex++;
@@ -127,10 +128,11 @@ public class DeliveryController implements ActionListener {
         }
         return data;
     }
-    private int getOrdersTableSize(){
+
+    private int getOrdersTableSize() {
         int size = orders.size();
-        for (Order order : orders){
-            if (order.getOrderLines().size() > 1){
+        for (Order order : orders) {
+            if (order.getOrderLines().size() > 1) {
                 size = size + order.getOrderLines().size() - 1;
             }
         }
@@ -139,7 +141,7 @@ public class DeliveryController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == terugButton){
+        if (e.getSource() == terugButton) {
             route.setStatus("Aannemen order");
             mainPanel.removeAll();
             KoerierController kController = new KoerierController(mainPanel);
@@ -147,7 +149,87 @@ public class DeliveryController implements ActionListener {
 
             mainPanel.revalidate();
             mainPanel.repaint();
+        } else if (e.getSource() == routeVoltooien) {
+            showRouteCompleteDialog();
         }
+
+    }
+
+    private void showRouteCompleteDialog() {
+        JDialog dialog = new JDialog((Frame) null, "Route voltooien", true);
+        dialog.setUndecorated(true);
+        dialog.setLayout(new GridBagLayout());
+        dialog.getRootPane().setBorder(new LineBorder(Color.BLACK, 2));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10,10,10,10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+
+        JLabel label1 = new JLabel("Aantal orders bezorgd: ");
+        JTextField ordersBezorgd = new JTextField("175");
+        ordersBezorgd.setEditable(false);
+        ordersBezorgd.setBackground(Color.WHITE);
+
+        JLabel label2 = new JLabel("Aantal orders terug naar magazijn: ");
+        JTextField ordersTerug = new JTextField("5");
+        ordersTerug.setEditable(false);
+        ordersTerug.setBackground(Color.WHITE);
+
+        JPanel panel1 = new JPanel(new BorderLayout());
+        panel1.add(label1, BorderLayout.NORTH);
+        panel1.add(ordersBezorgd, BorderLayout.CENTER);
+
+        JPanel panel2 = new JPanel(new BorderLayout());
+        panel2.add(label2, BorderLayout.NORTH);
+        panel2.add(ordersTerug, BorderLayout.CENTER);
+
+        dialog.add(panel1);
+        dialog.add(panel2);
+
+        JButton terugButton = new JButton("Ga Terug");
+        terugButton.setBackground(new Color(255,204,204));
+        terugButton.setOpaque(true);
+        terugButton.setBorderPainted(false);
+        terugButton.addActionListener(event -> dialog.dispose());
+
+        JButton doorsturenButton = new JButton("Doorsturen");
+        doorsturenButton.setBackground(new Color(204,255,204));
+        doorsturenButton.setOpaque(true);
+        doorsturenButton.setBorderPainted(false);
+        doorsturenButton.addActionListener(event -> {
+            //gooi de actie hier in dat ie terug naar t magazijn gaat
+            dialog.dispose();
+        });
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        dialog.add(label1, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        dialog.add(ordersBezorgd, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        dialog.add(label2, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        dialog.add(ordersTerug, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 15,0));
+        buttonPanel.add(terugButton);
+        buttonPanel.add(doorsturenButton);
+        dialog.add(buttonPanel, gbc);
+
+        dialog.setSize(400,200);
+        dialog.setLocationRelativeTo(mainPanel);
+        dialog.setVisible(true);
     }
 }
+
 
